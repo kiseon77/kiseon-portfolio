5eadcc39f6f4fad1737adaec00e8568f2e3bb6ba
kiseon77 <kieson.han77@gmail.com>
2024-12-18 23:37:04 +0900

    fix:폴더세팅

--- src/layouts/Nav.tsx
+++ src/layouts/Nav.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import "./nav.scss";
 export default function Nav() {
-  const [isOpen, setIsOpen] = useState<boolean>(false);
+  const [isOpen, _setIsOpen] = useState<boolean>(false);
   return (
     <nav className="menuContainer">
       <div className="menuBar">{isOpen ? "CLOSE" : "OPEN"}</div>
